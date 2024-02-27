083d62539a5774abb08c80820677b964ee180c11
Gerrit Koch <gerrit.koch@rwth-aachen.de>
2024-02-27 19:00:15 +0100

    small changes

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,6 +1,12 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString } from 'class-validator';
+import {
+    IsNotEmpty,
+    IsNumber,
+    IsOptional,
+    IsPositive,
+    IsString,
+} from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
     @IsNotEmpty({ message: 'first name must not be empty' })
